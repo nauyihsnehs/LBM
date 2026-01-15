ab8c57a87000701fca34b6b005452408fa494736
nauyihsnehs <13213058939@163.com>
2026-01-15 13:15:23 +0800

    delete old code

--- src/lbm/models/embedders/light_params/__init__.py
+++
@@ -1,4 +0,0 @@
-from .light_params_embedder_config import LightParamsEmbedderConfig
-from .light_params_embedder_model import LightParamsEmbedder
-
-__all__ = ["LightParamsEmbedder", "LightParamsEmbedderConfig"]
